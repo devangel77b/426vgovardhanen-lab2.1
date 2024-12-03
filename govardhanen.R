9076cb91d3470d5edb50910b1d62de8aae6782a3
Dr Dennis Evangelista <devangelista@frhsd.com>
2024-12-03 13:09:51 -0500

    blah

--- govardhanen.R
+++ govardhanen.R
@@ -1,5 +1,5 @@
 library(ggplot2)
-raw <- read.csv("avalur.csv",header=TRUE)
+raw <- read.csv("govardhanen.csv",header=TRUE)
 
 library(dplyr)
 data <- mutate(
@@ -11,15 +11,16 @@ apredicted.ms2 <- function(m2,m1,mc){
   9.81*m2/(m2+m1+mc)
 }
 
-fig <- ggplot(data)+geom_point(aes(x=m1.kg,y=ameas.ms2))+
-    ylim(0,2)+xlim(0,1)+
-    geom_function(fun=apredicted.ms2,args=list(m2=0.1,mc=0.5),color='blue')+
-    xlab('$m_1$ (\\unit{\\kilo\\gram})')+
+fig <- ggplot(data)+geom_point(aes(x=m2.kg,y=ameas.ms2))+
+    ylim(0,1.0)+xlim(0.020,0.1)+
+    geom_function(fun=apredicted.ms2,args=list(m1=0.,mc=0.5),color='blue')+
+    geom_function(fun=apredicted.ms2,args=list(m1=2.5,mc=0.5),color='red')+
+    xlab('$m_2$ (\\unit{\\kilo\\gram})')+
     ylab('$a$ (\\unit{\\meter\\per\\second\\squared})')+
     theme_bw(base_size=8)
 
 library(svglite)
-svglite('fig5.svg',width=3,height=2,pointsize=8)
+svglite('fig1.svg',width=3,height=2,pointsize=8)
 print(fig)
 dev.off()
 
@@ -29,10 +30,7 @@ results <- summarize(
 	sd.t = sd(t.s),
 	mean.a = mean(ameas.ms2),
 	sd.a = sd(ameas.ms2),
-	group_by(data,m1.kg)
+	group_by(data,m2.kg)
 	)
 print(xtable(results),include.rownames=FALSE,file='table1raw.tex')
 
-# for fun try a nonlinear fit of the data to find g and mc? 
-foo <- nls(ameas.ms2~g*m2.kg/(m1.kg+m2.kg+mc),data=data,start=c(g=9.81,mc=0.5))
-summary(foo)
